18fb7ed427b465724cd76e430479eab980b4ec69
dmdrk1414 <seungchan141414@gmail.com>
2022-11-11 19:05:43 +0900

    :sparkles:[Feat] 유저의 숫자만큼 유저를 생성한다.

--- src/UserControll.java
+++ src/UserControll.java
@@ -11,7 +11,15 @@ public class UserControll {
     }
 
     public void inputNumOfUser(Scanner sc) {
+        System.out.print("유저의 숫자는 몇명이야?");
         numUser = sc.nextInt();
     }
 
+    public void creatUser(User user, Scanner sc) {
+        for (int i = 0; i < numUser; i++) {
+            System.out.print((i + 1) + "번의 유저의 이름: ");
+            String name = sc.next();
+            userList.add(new User(name));
+        }
+    }
 }
