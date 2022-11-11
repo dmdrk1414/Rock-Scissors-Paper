a6fce9ff3f18cff341435fc6458207bf8dee82d3
dmdrk1414 <seungchan141414@gmail.com>
2022-11-11 18:32:50 +0900

    :sparkles:[Feat] 랜덤 손가락 할당하는 기능

--- src/Hand.java
+++ src/Hand.java
@@ -1,15 +1,36 @@
 import java.util.Random;
 
 public class Hand {
+    final static int rock = 0;
+    final static int scissor = 1;
+    final static int paper = 2;
+    String hand;
 
+    public Hand() {
+        hand = "";
+    }
 
-    public void getRandNum() {
-        int random = new Random().nextInt(3);
-        System.out.println(random);
+    public void setHandRandom() {
+        int numRandom = getRandNum();
+        switch (numRandom) {
+            case rock:
+                this.hand = "rock";
+                break;
+            case scissor:
+                this.hand = "scissor";
+                break;
+            case paper:
+                this.hand = "paper";
+                break;
+        }
+    }
+
+    public int getRandNum() {
+        return new Random().nextInt(3);
     }
 
     public static void main(String[] args) {
         Hand hand = new Hand();
-        hand.getRandNum();
+        hand.setHandRandom();
     }
 }
--- src/handEnum.java
+++
@@ -1,3 +0,0 @@
-public enum handEnum {
-    rock, scissor, paper
-}
