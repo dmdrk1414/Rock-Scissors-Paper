e65588f84156c9fe49a99520a219ce4fd04fb2e9
dmdrk1414 <seungchan141414@gmail.com>
2022-11-11 17:58:00 +0900

    :sparkles:[Feat] User 클래스 완성

--- src/User.java
+++ src/User.java
@@ -1,3 +1,13 @@
 public class User {
-    
+    String name;
+    Hand hand;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
