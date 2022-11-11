e3cfa78e2b7aff23d3e00b40bd241c1615d52eb6
dmdrk1414 <seungchan141414@gmail.com>
2022-11-11 18:10:50 +0900

    :sparkles:[Feat] 랜덤으로 숫자를 가져오는 메서드

--- src/Hand.java
+++ src/Hand.java
@@ -1,2 +1,15 @@
+import java.util.Random;
+
 public class Hand {
+
+
+    public void getRandNum() {
+        int random = new Random().nextInt(3);
+        System.out.println(random);
+    }
+
+    public static void main(String[] args) {
+        Hand hand = new Hand();
+        hand.getRandNum();
+    }
 }
---
+++ src/handEnum.java
@@ -0,0 +1,3 @@
+public enum handEnum {
+    rock, scissor, paper
+}
